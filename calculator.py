2b7a1768728c41e142a150bf5ca6bf19dca19af9
codedev-mi <shrutipb1601@gmail.com>
2024-08-12 21:36:40 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,15 +1,15 @@
 def calculator():
-     user_input = input(":")
-     # Check if the user wants to quit
-        if user_input == "quit":
-            break
+    while True:
+     # Get user input
+        user_input = input("Enter Operation: ")
+        
         # Check if the user input is a valid operator
-        elif user_input in ["+", "-", "*", "/"]:
+        if user_input in ["+", "-", "*", "/"]:
             # Get first number
             num1 = float(input("Enter First Number: "))
             # Get second number
             num2 = float(input("Enter Second Number: "))
-
+         
             # Perform the operation based on the user input
             if user_input == "+":
                 result = num1 + num2
@@ -26,10 +26,15 @@ def calculator():
             elif user_input == "/":
                 result = num1 / num2
                 print(num1, "/", num2, "=", result)
+            # check if user wants another calculation
+            # break the while loop if answer is no
+            next_input = input("exit calculation? (yes/no): ")
+            if next_input == "no":
+              break
+                
         else:
             # In case of invalid input
             print("Invalid Input")
 
 # Call the calculator function to start the program
 calculator()
-
